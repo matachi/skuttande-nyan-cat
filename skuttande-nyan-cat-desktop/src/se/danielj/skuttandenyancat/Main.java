41519c6311d490f02e730207582f032b5211e2fd
MaTachi <matachi@matachi-System-Product-Name.(none)>
2012-12-30 18:50:11 +0100

    Init commit

---
+++ skuttande-nyan-cat-desktop/src/se/danielj/skuttandenyancat/Main.java
@@ -0,0 +1,16 @@
+package se.danielj.skuttandenyancat;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "skuttande-nyan-cat";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Game(), cfg);
+	}
+}
